23c3a9115bba1c335df3f738fd3cd8d0a9ffb3d1
hezhe <hezhe@ihangmei.com>
2017-04-19 15:37:11 +0800

    update

--- js/page/myInfo.js
+++ js/page/myInfo.js
@@ -2,21 +2,38 @@ import "css/credentials.scss";
 
 import Vue from "vue";
 import commonTop from "common-top";
+import {getN} from "nativeA";
 
 window.addEventListener("DOMContentLoaded",()=>{
+    const BASEINFO = getN('baseInfo');
     new Vue({
         el: "#myInfo",
         data: {
-            company: '宇通客车',
-            driverName: '张三',
-            driverTel: '15000000000',
-            driverNumber: '111111',
-            driverPic: '../images/tmp/zhengjian4.png'
+            cmp_name: '',
+            name: '',
+            mobile: '',
+            license: '',
+            license_photo: '' // '../images/tmp/zhengjian4.png'
         },
         mounted(){
-
+            this.getUserInfo();
         },
         methods: {
+            getUserInfo(){
+                fetch(`${BASEINFO.host}/app-dms/driver/getUserInfo?uid=${BASEINFO.uid}`)
+                    .then(response=>response.json())
+                    .then(message=>{
+                        if (message.code==0) {
+                            let data = message.data;
+                            this.cmp_name = data.cmp_name;
+                            this.name = data.name;
+                            this.mobile = data.mobile;
+                            this.license = data.license;
+                            this.license_photo = data.license_photo;
+                        }
+                    })
+                    .catch(e=>console.log(e));
+            }
         },
         components: {
             commonTop
--- js/page/settings.js
+++ js/page/settings.js
@@ -1,30 +1,66 @@
 import "css/settings.scss";
 import Vue from "vue";
 import commonTop from "common-top";
+import {getN,callN} from "nativeA";
+import msg from "msg";
+import eventHub from "eventHub";
 
 window.addEventListener("DOMContentLoaded",()=>{
 
     new Vue({
         el: "#settings",
         data:{
+            cache: "",
             isCanUpdate: false
         },
         mounted(){
             this.isCanUpdate = true;
+            this.cache = getN("getCache").size;
+
+            eventHub.$on("msg-confirm",(content,type)=>{
+                switch(type){
+                    case 1:
+                        callN('cleanCache');
+                        this.cache = "0M";
+                        break;
+                    case 2:
+                        callN('updateApp');
+                        break;
+                    case 3:
+                        callN('signOut');
+                        break;
+                }
+            });
         },
         methods: {
+            getCanupdata(){
+                fetch('',{
+                    cache: "no-cache"
+                })
+                    .then(response=>response.json())
+                    .then(message=>{
+                        console.log(message);
+                    })
+                    .cache(e=>console.log(e));
+            },
             cleanCache(){
-
+                eventHub.$emit("msg-show","确定清除本地缓存？",1);
             },
             updateApp(){
-
+                // 当前为最新版本
+                if (this.isCanUpdate) {
+                    eventHub.$emit("msg-show","检测到新版本，是否确定更新？",2);
+                }else{
+                    alert('当前为最新版本');
+                }
             },
             signOut(){
-                
+                eventHub.$emit("msg-show","确定注销？",3);
             }
         },
         components: {
-            commonTop
+            commonTop,
+            msg
         }
     })
 },false);
