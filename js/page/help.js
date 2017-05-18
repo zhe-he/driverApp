d7daa497b30fc72129e1c262f737acc29bd1e47e
zhe-he <hezhe@ihangmei.com>
2017-05-18 16:56:55 +0800

    update help

--- js/page/help.js
+++ js/page/help.js
@@ -1,1 +1,13 @@
-import "css/help.scss";
+import "css/aboutUs.scss";
+import Vue from "vue";
+import commonTop from "common-top";
+
+window.addEventListener("DOMContentLoaded",()=>{
+
+    new Vue({
+        el: "#help",
+        components: {
+            commonTop
+        }
+    })
+},false);
